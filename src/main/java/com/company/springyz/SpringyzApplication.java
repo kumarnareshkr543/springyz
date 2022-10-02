d755f4cc46f1d9bec8252e53a6ec970ff147a981
naresh003kr <naresh.003kr@gmail.com>
2022-10-02 14:51:43 +0530

    First Commit1

--- src/main/java/com/company/springyz/SpringyzApplication.java
+++ src/main/java/com/company/springyz/SpringyzApplication.java
@@ -8,6 +8,7 @@ public class SpringyzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringyzApplication.class, args);
+        System.out.println("kkkk");
     }
 
 }
