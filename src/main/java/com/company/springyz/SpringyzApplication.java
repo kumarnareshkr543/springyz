1e6523e8ca9dba08a06dbe0a61e7bef00f3e7a2a
naresh003kr <naresh.003kr@gmail.com>
2022-10-02 14:41:54 +0530

    First Commit

---
+++ src/main/java/com/company/springyz/SpringyzApplication.java
@@ -0,0 +1,13 @@
+package com.company.springyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringyzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringyzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/springyz/SpringyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
